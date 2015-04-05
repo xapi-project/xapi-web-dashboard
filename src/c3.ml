d8a028077d4c008aaab5b5e8bc67dbd131ad1437
David Scott <dave.scott@citrix.com>
2015-04-05 19:12:34 +0100

    C3: add column types (area, line, area-spline)
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- src/c3.ml
+++ src/c3.ml
@@ -1,15 +1,26 @@
+type column_type =
+  | Line
+  | Area
+  | Area_spline
 
 type column = {
   label: string;
   values: float list;
+  ty: column_type;
 }
 
 type data = { columns: column list }
 
 let example = {
   columns = [
-    { label = "data1"; values = [ 30.; 200.; 100.; 400.; 150.; 250. ] };
-    { label = "data2"; values = [ 50.; 20.; 10.; 40.; 15.; 25. ] }
+    { label = "data1";
+      values = [ 30.; 200.; 100.; 400.; 150.; 250. ];
+      ty = Area;
+    }; {
+      label = "data2";
+      values = [ 50.; 20.; 10.; 40.; 15.; 25. ];
+      ty = Line;
+    }
   ]
 }
 
@@ -21,11 +32,20 @@ var chart = c3.generate({
     columns: [
       ['data1', 30, 200, 100, 400, 150, 250],
       ['data2', 50, 20, 10, 40, 15, 25]
-    ]
+    ],
+    types: {
+      data1: 'area',
+      data2: 'area-spline'
+    }
   }
 });
 
 *)
+
+let string_of_column_type = function
+  | Line -> "line"
+  | Area -> "area"
+  | Area_spline -> "area-spline"
 
 let generate bindto data =
   let arg =
@@ -34,8 +54,11 @@ let generate bindto data =
         "data", obj [|
           "columns", inject (Js.array (Array.of_list (List.map (fun column ->
             Js.array (Array.of_list (inject (Js.string column.label) :: (List.map inject column.values)))
-          ) data.columns)))
-        |]
+          ) data.columns)));
+          "types", obj (Array.of_list (List.map (fun column ->
+            column.label, inject (Js.string (string_of_column_type column.ty))
+          ) data.columns));
+        |];
       |])  in
   let c3 = Js.Unsafe.global##c3 in
 
